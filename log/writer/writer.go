a580f436364190edf2e10d8120f2e58f97e6efb2
Al Cutter <al@google.com>
2024-04-22 14:25:12 +0100

    Tidy up pool flushing

--- log/writer/writer.go
+++ log/writer/writer.go
@@ -19,7 +19,6 @@ func NewPool(bufferSize int, maxAge time.Duration, s SequenceFunc) *Pool {
 	return &Pool{
 		current: &batch{
 			Done: make(chan struct{}),
-			Born: time.Now(),
 		},
 		bufferSize: bufferSize,
 		seq:        s,
@@ -43,6 +42,7 @@ type Pool struct {
 func (p *Pool) Add(e []byte) (uint64, error) {
 	p.Lock()
 	b := p.current
+	// If this is the first entry in a batch, set a flush timer so we attempt to sequence it within maxAge.
 	if len(b.Entries) == 0 {
 		p.flushTimer = time.AfterFunc(p.maxAge, func() {
 			p.Lock()
@@ -51,7 +51,8 @@ func (p *Pool) Add(e []byte) (uint64, error) {
 		})
 	}
 	n := b.Add(e)
-	if n >= p.bufferSize || time.Since(p.current.Born) > p.maxAge {
+	// If the batch is full, then attempt to sequence it immediately.
+	if n >= p.bufferSize {
 		p.flushWithLock()
 	}
 	p.Unlock()
@@ -60,12 +61,16 @@ func (p *Pool) Add(e []byte) (uint64, error) {
 }
 
 func (p *Pool) flushWithLock() {
+	// timer can be nil if a batch was flushed because it because full at about the same time as it hit maxAge.
+	// In this case we can just return.
+	if p.flushTimer == nil {
+		return
+	}
 	p.flushTimer.Stop()
 	p.flushTimer = nil
 	b := p.current
 	p.current = &batch{
 		Done: make(chan struct{}),
-		Born: time.Now(),
 	}
 	go func() {
 		b.FirstSeq, b.Err = p.seq(context.TODO(), Batch{Entries: b.Entries})
@@ -74,7 +79,6 @@ func (p *Pool) flushWithLock() {
 }
 
 type batch struct {
-	Born     time.Time
 	Entries  [][]byte
 	Done     chan struct{}
 	FirstSeq uint64
